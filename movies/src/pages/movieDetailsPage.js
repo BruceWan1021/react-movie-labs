58cc913c4a6949b7b426fe88ee899a45c7f302d6
BruceWan1021 <2583809090@qq.com>
2024-10-15 14:41:59 +0100

    Refactor - useMovie custom hook.

--- movies/src/pages/movieDetailsPage.js
+++ movies/src/pages/movieDetailsPage.js
@@ -1,96 +1,129 @@
-// // import React from "react";
-// import MovieHeader from "../components/headerMovie/";
-// import MovieDetails from "../components/movieDetails/";
-// import Grid from "@mui/material/Grid2";
-// import ImageList from "@mui/material/ImageList";
-// import ImageListItem from "@mui/material/ImageListItem";
+// // // import React from "react";
+// // import MovieHeader from "../components/headerMovie/";
+// // import MovieDetails from "../components/movieDetails/";
+// // import Grid from "@mui/material/Grid2";
+// // import ImageList from "@mui/material/ImageList";
+// // import ImageListItem from "@mui/material/ImageListItem";
+// // import React, {useState, useEffect}  from "react";
+// // import { useParams } from 'react-router-dom';
+// // import { getMovie, getMovieImages } from "../api/tmdb-api";
+
+
+// // const MoviePage = (props) => {
+// //     const { id } = useParams();
+// //     const [movie, setMovie] = useState(null);
+// //     const [images, setImages] = useState([]);
+  
+// //     // useEffect(() => {
+// //     //   fetch(
+// //     //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
+// //     //   )
+// //     //     .then((res) => {
+// //     //       return res.json();
+// //     //     })
+// //     //     .then((movie) => {
+// //     //       // console.log(movie)
+// //     //       setMovie(movie);
+// //     //     });
+// //     // }, [id]);
+  
+// //     // useEffect(() => {
+// //     //   fetch(
+// //     //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
+// //     //   )
+// //     //     .then((res) => res.json())
+// //     //     .then((json) => json.posters)
+// //     //     .then((images) => {
+// //     //       // console.log(images)
+// //     //       setImages(images);
+// //     //     });
+// //     //     // eslint-disable-next-line
+// //     // }, []);
+// //     useEffect(() => {
+// //         getMovie(id).then((movie) => {
+// //           setMovie(movie);
+// //         });
+// //       }, [id]);
+    
+// //       useEffect(() => {
+// //         getMovieImages(id).then((images) => {
+// //           setImages(images);
+// //         });
+// //         // eslint-disable-next-line react-hooks/exhaustive-deps
+// //       }, []);
+
+// //   return (
+// //     <>
+// //       {movie ? (
+// //         <>
+// //           <MovieHeader movie={movie} />
+// //           <Grid container spacing={5} style={{ padding: "15px" }}>
+// //             <Grid size={{xs: 3}}>
+// //               <div sx={{
+// //                 display: "flex",
+// //                 flexWrap: "wrap",
+// //                 justifyContent: "space-around",
+// //               }}>
+// //                 <ImageList
+// //                     sx={{
+// //                         height: "100vh",
+// //                     }}
+// //                     cols={1}
+// //                 >
+// //                     {images.map((image) => (
+// //                         <ImageListItem
+// //                         key={image.file_path}
+// //                         cols={1}
+// //                         >
+// //                             <img
+// //                                 src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
+// //                                 alt={image.file_path}
+// //                             />
+// //                         </ImageListItem>
+// //                     ))}
+// //                 </ImageList>
+// //               </div>
+// //             </Grid>
+// //             <Grid size={{xs: 9}}>
+// //               <MovieDetails movie={movie} />
+// //             </Grid>
+// //           </Grid>
+// //         </>
+// //       ) : (
+// //         <h2>Waiting for API data</h2>
+// //       )}
+// //     </>
+// //   );
+// // };
+
+// // export default MoviePage;
+
 // import React, {useState, useEffect}  from "react";
 // import { useParams } from 'react-router-dom';
-// import { getMovie, getMovieImages } from "../api/tmdb-api";
-
+// import MovieDetails from "../components/movieDetails/";
+// import PageTemplate from "../components/templateMoviePage";
+// import { getMovie } from "../api/tmdb-api";
 
 // const MoviePage = (props) => {
-//     const { id } = useParams();
-//     const [movie, setMovie] = useState(null);
-//     const [images, setImages] = useState([]);
-  
-//     // useEffect(() => {
-//     //   fetch(
-//     //     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_TMDB_KEY}`
-//     //   )
-//     //     .then((res) => {
-//     //       return res.json();
-//     //     })
-//     //     .then((movie) => {
-//     //       // console.log(movie)
-//     //       setMovie(movie);
-//     //     });
-//     // }, [id]);
-  
-//     // useEffect(() => {
-//     //   fetch(
-//     //     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${process.env.REACT_APP_TMDB_KEY}`
-//     //   )
-//     //     .then((res) => res.json())
-//     //     .then((json) => json.posters)
-//     //     .then((images) => {
-//     //       // console.log(images)
-//     //       setImages(images);
-//     //     });
-//     //     // eslint-disable-next-line
-//     // }, []);
-//     useEffect(() => {
-//         getMovie(id).then((movie) => {
-//           setMovie(movie);
-//         });
-//       }, [id]);
-    
-//       useEffect(() => {
-//         getMovieImages(id).then((images) => {
-//           setImages(images);
-//         });
-//         // eslint-disable-next-line react-hooks/exhaustive-deps
-//       }, []);
+//   const { id } = useParams();
+//   const [movie, setMovie] = useState(null);
+
+//   useEffect(() => {
+//     getMovie(id).then((movie) => {
+//       setMovie(movie);
+//     });
+//   }, [id]);
 
 //   return (
 //     <>
 //       {movie ? (
 //         <>
-//           <MovieHeader movie={movie} />
-//           <Grid container spacing={5} style={{ padding: "15px" }}>
-//             <Grid size={{xs: 3}}>
-//               <div sx={{
-//                 display: "flex",
-//                 flexWrap: "wrap",
-//                 justifyContent: "space-around",
-//               }}>
-//                 <ImageList
-//                     sx={{
-//                         height: "100vh",
-//                     }}
-//                     cols={1}
-//                 >
-//                     {images.map((image) => (
-//                         <ImageListItem
-//                         key={image.file_path}
-//                         cols={1}
-//                         >
-//                             <img
-//                                 src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-//                                 alt={image.file_path}
-//                             />
-//                         </ImageListItem>
-//                     ))}
-//                 </ImageList>
-//               </div>
-//             </Grid>
-//             <Grid size={{xs: 9}}>
-//               <MovieDetails movie={movie} />
-//             </Grid>
-//           </Grid>
+//           <PageTemplate movie={movie}>
+//             <MovieDetails movie={movie} />
+//           </PageTemplate>
 //         </>
 //       ) : (
-//         <h2>Waiting for API data</h2>
+//         <p>Waiting for movie details</p>
 //       )}
 //     </>
 //   );
@@ -98,21 +131,15 @@
 
 // export default MoviePage;
 
-import React, {useState, useEffect}  from "react";
+import React from "react";
 import { useParams } from 'react-router-dom';
 import MovieDetails from "../components/movieDetails/";
 import PageTemplate from "../components/templateMoviePage";
-import { getMovie } from "../api/tmdb-api";
+import useMovie from "../hooks/useMovie";
 
 const MoviePage = (props) => {
   const { id } = useParams();
-  const [movie, setMovie] = useState(null);
-
-  useEffect(() => {
-    getMovie(id).then((movie) => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const [movie] = useMovie(id);
 
   return (
     <>
