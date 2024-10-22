256e312948e73d467b50f1de928383761e8cb16c
BruceWan1021 <2583809090@qq.com>
2024-10-22 14:04:33 +0100

    Using react-query to manage the browser-based cache.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -65,21 +65,21 @@
 // };
 // export default HomePage;
 
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage';
-import { getMovies } from "../api/tmdb-api";
+// import React, { useState, useEffect } from "react";
+// import PageTemplate from '../components/templateMovieListPage';
+// import { getMovies } from "../api/tmdb-api";
 
-const HomePage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+// const HomePage = (props) => {
+//   const [movies, setMovies] = useState([]);
+//   const favorites = movies.filter(m => m.favorite)
+//   localStorage.setItem('favorites', JSON.stringify(favorites))
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+//   const addToFavorites = (movieId) => {
+//     const updatedMovies = movies.map((m) =>
+//       m.id === movieId ? { ...m, favorite: true } : m
+//     );
+//     setMovies(updatedMovies);
+//   };
 
   // useEffect(() => {
   //   fetch(
@@ -94,11 +94,45 @@ const HomePage = (props) => {
   //     });
   // }, []);
 
-  useEffect(() => {
-    getMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+//   useEffect(() => {
+//     getMovies().then(movies => {
+//       setMovies(movies);
+//     });
+//   }, []);
+
+//   return (
+//     <PageTemplate
+//       title='Discover Movies'
+//       movies={movies}
+//       selectFavorite={addToFavorites}
+//     />
+//   );
+// };
+// export default HomePage;
+
+import React from "react";
+import { getMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+
+const HomePage = (props) => {
+
+  const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter(m => m.favorite)
+  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const addToFavorites = (movieId) => true 
 
   return (
     <PageTemplate
